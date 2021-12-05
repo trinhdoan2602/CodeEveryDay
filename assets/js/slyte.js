b99177d376539a7db8403d7c21f259451938be44
trinhdoan2602 <doancongtrinh6655@gmail.com>
2021-12-05 09:51:44 +0700

    Clear landingpage

--- assets/js/slyte.js
+++ assets/js/slyte.js
@@ -5,3 +5,34 @@ menuBar.addEventListener("click", function(){
     document.querySelector("#menu").classList.toggle("active")
     document.querySelector(".header-right").classList.toggle("active")
 })
+
+// slider-slick
+$('.responsive1').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  
