f00fa3d1867393cbdc74249e942ab55a8b00f53e
trinhdoan2602 <doancongtrinh6655@gmail.com>
2021-12-06 16:28:38 +0700

    Add courses and clear landing page

--- assets/js/slyte.js
+++ assets/js/slyte.js
@@ -12,32 +12,91 @@ signIn.addEventListener("click", function(){
 })
 
 // slider-slick
-$('.responsive1').slick({
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+$('.responsive').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1424,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
+    },
+    {
+      breakpoint: 1124,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 874,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.responsive1').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+//course menu
+const courseTitle = document.querySelector(".course-title");
+courseTitle.addEventListener("click", function(x){
+    if(x.target.classList.contains("course-button")){
+        const Target = x.target.getAttribute("data-title");
+        // console.log(Target)
+        courseTitle.querySelector(".active").classList.remove("active");
+        x.target.classList.add("active");
+        const courseItem = document.querySelector(".courses");
+        courseItem.querySelector(".course-main.active").classList.remove("active");
+        courseItem.querySelector(Target).classList.add("active");
+    }
+    
+})
+
 
   
